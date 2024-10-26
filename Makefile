dfed9cccd7b51e76e5863ed00a67847e852a678a
Ahhyun Moon <am12180@nyu.edu>
2024-10-26 14:41:57 -0400

    Add project setup

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 COMPOSE_FILE = compose.yaml
 
 up:
@@ -14,3 +15,21 @@ down:
 	
 cli:
 	docker exec -it database sqlite3
+=======
+COMPOSE_FILE = compose.yaml
+
+up:
+	docker-compose -f $(COMPOSE_FILE) up -d
+	
+stop:
+	docker-compose -f $(COMPOSE_FILE) stop
+	
+start:
+	docker-compose -f $(COMPOSE_FILE) start
+	
+down:
+	docker-compose -f $(COMPOSE_FILE) down
+	
+cli:
+	docker exec -it database sqlite3
+>>>>>>> 6daa86c (Add project setup)
--- database/sample.sql
+++ database/sample.sql
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, age INTEGER NOT NULL);
 
 INSERT INTO users (name, age) VALUES 
 ('Alice', 30),
 ('Bob', 25),
 ('Charlie', 28);
+=======
+CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, age INTEGER NOT NULL);
+
+INSERT INTO users (name, age) VALUES 
+('Alice', 30),
+('Bob', 25),
+('Charlie', 28);
+>>>>>>> 6daa86c (Add project setup)
