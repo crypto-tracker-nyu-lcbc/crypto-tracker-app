277846aef6fe79b7e4e960d934411b71643556b0
Ahhyun Moon <am12180@nyu.edu>
2024-10-26 15:53:37 -0400

    Complete setup

--- crypto-tracker-app/api/api.py
+++ crypto-tracker-app/api/api.py
@@ -1,8 +1,10 @@
-import time
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:3000"}})
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
-    return {'time': time.time()}
+    current_time = 12345
+    return {'time': current_time}
