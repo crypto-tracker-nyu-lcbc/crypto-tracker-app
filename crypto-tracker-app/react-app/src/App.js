15154c1b349da15192e18c815f30168bad8d41ff
Ahhyun Moon <am12180@nyu.edu>
2024-11-13 16:35:38 -0500

    Added nav bar and refactored app.js

--- crypto-tracker-app/react-app/src/App.js
+++ crypto-tracker-app/react-app/src/App.js
@@ -1,16 +1,40 @@
 import React from "react";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import SearchBar from "./SearchBar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Menu from "./Menu";
 import SearchResult from "./SearchResult";
+import Home from "./Home";
+
+const theme = createTheme({
+    // backgroundColor: "#1b2139",
+    // color: "#a4edf0",
+    palette: {
+        primary: {
+            main: "#102ff8",
+        },
+        secondary: {
+            main: "#ff5903",
+        },
+    },
+    typography: {
+        fontFamily: "Space Grotesk",
+    },
+    // palette: {
+    //     primary: "#1095ba",
+    //     secondary: "#e67488",
+    // },
+});
 
 function App() {
     return (
-        <div>
+        <ThemeProvider theme={theme}>
+            <Menu />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/search" element={<SearchResult />} />
-                <Route path="/" element={<SearchBar />} />
             </Routes>
-        </div>
+        </ThemeProvider>
     );
 }
 
